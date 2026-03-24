36cccc75fc51e4e884e5632afa92d2f0a0aeff9b
Nhat Nguyen <quangnhatnguyen27506@gmail.com>
2026-03-24 17:14:14 +0700

    Hoan thanh

--- src/Lab01/Bai_222_FirstDialog.java
+++ src/Lab01/Bai_222_FirstDialog.java
@@ -1,6 +1,6 @@
 package Lab01;
 
-import javax.swing.JOptionPane; // Tao hoi thoai
+import javax.swing.JOptionPane; // Ham tao hoi thoai
 public class Bai_222_FirstDialog {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Hello world! How are you?");
