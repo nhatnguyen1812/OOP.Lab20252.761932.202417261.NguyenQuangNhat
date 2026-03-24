3083da3153de6c9f275d71f30d72f1c6855f6129
Nhat Nguyen <quangnhatnguyen27506@gmail.com>
2026-03-24 17:02:41 +0700

    Hoan thanh bai 222

--- src/Lab01/Bai_221_HelloWorld.java
+++ src/Lab01/Bai_221_HelloWorld.java
@@ -1,6 +1,6 @@
 package Lab01;
 
-public class HelloWorld {
+public class Bai_221_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Xin chao \n cac ban!");
         System.out.println("Hello \t world!");
---
+++ src/Lab01/Bai_222_FirstDialog.java
@@ -0,0 +1,10 @@
+package Lab01;
+
+import javax.swing.JOptionPane; // Tao hoi thoai
+public class Bai_222_FirstDialog {
+    public static void main(String[] args) {
+        // Lệnh hiện hộp thoại thông báo
+        JOptionPane.showMessageDialog(null, "Hello world! How are you?");
+        System.exit(0);
+    }
+}
